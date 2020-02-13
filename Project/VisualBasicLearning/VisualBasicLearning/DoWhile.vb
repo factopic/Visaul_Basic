5184599d3c0868f743aad038e4a27c213831f9a4
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2020-02-13 22:45:13 +0530

    Update DoWhile.vb

--- Project/VisualBasicLearning/VisualBasicLearning/DoWhile.vb
+++ Project/VisualBasicLearning/VisualBasicLearning/DoWhile.vb
@@ -1,3 +1,10 @@
 ﻿Public Class DoWhile
-
+  Public count As Integer
+  Sub Button1_Click() Handles Button1.Click
+    Do While
+      TextBox1.Text = count
+      count++
+    End while
+      
+  End Sub
 End Class
